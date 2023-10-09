073b20949c7e24730c2d6d713d94e980bd963f78
mtalhahabib <talhahabib099@gmail.com>
2023-10-09 17:15:22 +0500

    updated

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -28,7 +28,8 @@ class MyHomePage extends StatelessWidget {
 
       ),
       drawer: MyDrawer(),
-      body: SingleChildScrollView(
+      body:
+       SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 20,right: 20),
           child: Column(
